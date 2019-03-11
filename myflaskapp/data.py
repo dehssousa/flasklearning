cbd8df17a059f86b69b5b3b69fbf78d5bfa1255f
dehssousa <30813714+dehssousa@users.noreply.github.com>
2019-03-11 11:07:01 -0300

    Add files via upload

---
+++ myflaskapp/data.py
@@ -0,0 +1,25 @@
+def Articles():
+    articles = [
+        {
+            'id': 1,
+            'title':'A-Squad',
+            'body': 'Lorem ipsum blablablblalblalblalblalab',
+            'Author': 'Bradley Cooper',
+            'created date':'04-12-2013'
+        },
+         {
+            'id': 2,
+            'title':'A Start is Born',
+            'body': 'Lorem ipsum blablablblalblalblalblalab',
+            'Author': 'Lady Gaga',
+            'created date':'04-11-2013'
+        },
+         {
+            'id': 3,
+            'title':'The first man',
+            'body': 'Lorem ipsum blablablblalblalblalblalab',
+            'Author': 'Rian Gosling',
+            'created date':'04-32-2013'
+        },
+    ]
+    return articles 

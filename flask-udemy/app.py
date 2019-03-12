87faca7b2dbcccae59e52f222bb05124a7d4d281
Andre Sousa <dehssousa@gmail.com>
2019-03-12 11:00:15 -0300

    Modulo 1

--- flask-udemy/app.py
+++ flask-udemy/app.py
@@ -4,8 +4,6 @@ app = Flask("Projeto")
 
 @app.route("/")
 def ola_mundo():
-    return "Oi, meu chapa", 200
+    return render_template("index.html", meu_nome="Texugo"), 200
 
-app.run()
-
-#test
+app.run(debug=True)

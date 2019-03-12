22e2b72a3279bfafd5b637750fd3438a9d63a6ba
Andre Sousa <dehssousa@gmail.com>
2019-03-12 16:30:49 -0300

    Curso

--- flask-udemy/app.py
+++ flask-udemy/app.py
@@ -4,6 +4,22 @@ app = Flask("Projeto")
 
 @app.route("/")
 def ola_mundo():
-    return render_template("index.html", meu_nome="Texugo"), 200
+    return render_template("index.html"), 200
+
+@app.route("/getype")
+def go_get():
+    return render_template("get.html"), 200
+
+@app.route("/getpost")
+def go_post():
+    return render_template("post.html"), 200
+    
+@app.route("/about")
+@app.route("/about/<name>")
+@app.route("/about/<name>/<age>")
+def about(name = None, age = None):
+    return "Maciota conhece o {} de {} anos".format(name,age), 200
+
+
 
 app.run(debug=True)
